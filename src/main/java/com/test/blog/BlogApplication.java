a0e6b58d2449558d30cf39d59934d68d8199be03
snailmai <1033479811@qq.com>
2021-03-24 23:27:15 +0800

    优化

--- src/main/java/com/test/blog/BlogApplication.java
+++ src/main/java/com/test/blog/BlogApplication.java
@@ -1,15 +1,11 @@
 package com.test.blog;
 
-import com.test.blog.entity.dto.BlogUserWrap;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class BlogApplication { // extends 是为了打包
+public class BlogApplication extends ServletInitializer{ // extends 是为了打包
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
--- src/main/java/com/test/blog/ServletInitializer.java
+++ src/main/java/com/test/blog/ServletInitializer.java
@@ -1,20 +1,20 @@
 package com.test.blog;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-//@SpringBootApplication
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
+	// 打包需要增加的内容
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(BlogApplication.class);
 	}
 
-	public static void main(String[] args){
-		SpringApplication.run(BlogApplication.class, args);
-	}
+//	public static void main(String[] args){
+//		SpringApplication.run(BlogApplication.class, args);
+//	}
 
 }

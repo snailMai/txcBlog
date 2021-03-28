4c66d5ce2d5bada092736293968a90b6c310bc96
snailmai <1033479811@qq.com>
2021-03-28 22:58:58 +0800

    testUser complete

--- src/main/java/com/test/blog/testfortestuser/controller/TestUserController.java
+++ src/main/java/com/test/blog/testfortestuser/controller/TestUserController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 
 
 /**
@@ -24,56 +25,102 @@ public class TestUserController {
     TestUserMapper testUserMapper;
 
     @RequestMapping(value = "pic")
-    public TestUser pic() throws Exception{
-        log.debug("-----------debug----------");
-        log.info("-----------pic----------");
+    public TestUser pic() {
+
         try {
             // 调用dao层
             TestUser testUser = testUserMapper.selectUserByName("wx");
+            log.debug("-----------debug----------");
+            log.info("-----------pic----------");
             return testUser;
         }catch (Exception e){
             System.out.println("call method failed,error: " + e);
+            log.error("call error: " + e);
         }
-
 
         return null;
     }
 
-    @RequestMapping(value = "", method = RequestMethod.POST, produces="test/html;charset=UTF-8")
-    public TestUser addTestUser(@PathVariable String username, @PathVariable Integer age) throws Exception{
+    @RequestMapping(value = "{username}_{age}", method = RequestMethod.POST)
+    public TestUser addTestUser(@PathVariable String username, @PathVariable Integer age) {
+        Integer testUserNumber = testUserMapper.countTestUser();
+
+        if (testUserNumber > 100){
+            log.error("addUser failed: the user reach the limit");
+            return null;
+        }
         TestUser testUser = new TestUser();
         try{
             testUser = testUserMapper.insertTestUser(username, age);
+            log.debug("-----------debug----------");
+            log.info("-----------addTestUser----------");
+            log.info("add user success, the user is " + username);
             return testUser;
         }catch (Exception e){
             System.out.println("call method failed,error: " + e);
+            log.error("call error: " + e);
         }
         return null;
     }
 
-//    @RequestMapping(value = "{username}", method = RequestMethod.GET, produces = "test/html;charset=UTF-8")
     @RequestMapping(value = "{username}", method = RequestMethod.GET)
-    public TestUser getTestUserByName(@PathVariable String username ) throws  Exception{
+    public TestUser getTestUserByName(@PathVariable String username ) {
         TestUser testUser = new TestUser();
         try{
             testUser = testUserMapper.selectUserByName(username);
+            log.debug("-----------debug----------");
+            log.info("----------getTestUserByName-----------");
             return testUser;
+        }catch (Exception e){
+            System.out.println("call method failed,error: " + e);
+            log.error("call error: " + e);
+        }
+        return null;
+    }
+
+    @RequestMapping(value = "{username}", method = RequestMethod.DELETE)
+    public TestUser deleteTestUserByName(@PathVariable String username ) {
+        TestUser testUser = new TestUser();
+        try{
+            testUser = testUserMapper.deleteTestUser(username);
+            log.debug("-----------debug----------");
+            log.info("----------deleteTestUserByName-----------");
+            log.info("delete user success, the user is " + username);
+            return testUser;
+        }catch (Exception e){
+            System.out.println("call method failed,error: " + e);
+            log.error("call error: " + e);
+        }
+        return null;
+    }
+
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public ArrayList<TestUser> getAllTestUser() throws  Exception{
+        ArrayList<TestUser> listTestUser;
+        try{
+            listTestUser = testUserMapper.findAllTestUser();
+            log.debug("-----------debug----------");
+            log.info("----------getAllTestUser-----------");
+            return listTestUser;
         }catch (Exception e){
             System.out.println("call method failed,error: " + e);
         }
         return null;
     }
 
-//    @RequestMapping(value = "{username}", method = RequestMethod.GET, produces = "test/html;charset=UTF-8")
-//    public TestUser getAllTestUser() throws  Exception{
-//        TestUser testUser = new TestUser();
-//        try{
-//            testUser = testUserMapper.selectUserByName(username);
-//            return testUser;
-//        }catch (Exception e){
-//            System.out.println("call method failed,error: " + e);
-//        }
-//        return null;
-//    }
+    @RequestMapping(value = "/number", method = RequestMethod.GET)
+    public Integer getTestUserNumber() throws  Exception{
+
+        try{
+            Integer testUserNumber = testUserMapper.countTestUser();
+            log.debug("-----------debug----------");
+            log.info("----------getTestUserNumber-----------");
+            return testUserNumber;
+        }catch (Exception e){
+            System.out.println("call method failed,error: " + e);
+            log.error("call error: " + e);
+        }
+        return null;
+    }
 
 }
--- src/main/java/com/test/blog/testfortestuser/dao/TestUserMapper.java
+++ src/main/java/com/test/blog/testfortestuser/dao/TestUserMapper.java
@@ -4,11 +4,15 @@ import com.test.blog.testfortestuser.domain.TestUser;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Mapper
 @Repository
 public interface TestUserMapper {
     TestUser insertTestUser(String username, Integer age);
     TestUser deleteTestUser(String username);
     TestUser selectUserByName(String username);
+    ArrayList<TestUser> findAllTestUser();
+    Integer countTestUser();
 //    TestUser
 }

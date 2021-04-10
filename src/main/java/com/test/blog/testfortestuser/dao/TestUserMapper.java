cfeaaae94e24c2dd1feed3f4697620a321ba46bf
snailmai <1033479811@qq.com>
2021-04-10 18:31:17 +0800

    注释

--- src/main/java/com/test/blog/testfortestuser/dao/TestUserMapper.java
+++ src/main/java/com/test/blog/testfortestuser/dao/TestUserMapper.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 @Mapper
 @Repository
 public interface TestUserMapper {
+    // 实际返回的testUser是要在service层实现,这里直接查询sql返回,一般mapper返回参数是int,来确定sql语句有没有成功
     TestUser insertTestUser(String username, Integer age);
     TestUser deleteTestUser(String username);
     TestUser selectUserByName(String username);
